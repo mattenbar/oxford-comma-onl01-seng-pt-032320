b5cafbe4778e09fe0f9b95f280e1bb1a7a377e26
matthew enbar <matthew.enbar@gmail.com>
2020-03-24 21:31:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+ else
+   last_element = ", and #{array.pop}"
+   strin_array = array.join(", ")
+   return "#{strin_array}#{last_element}"
+  end
 end
